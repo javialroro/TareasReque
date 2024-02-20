25f9ec08b5c38fb8bc4c5058a0092ed69751d02b
javialroro <102406440+javialroro@users.noreply.github.com>
2024-02-19 19:35:51 -0600

    pantallaBotones

--- Tarea01/app/src/main/java/com/example/tarea01/MainActivity.kt
+++ Tarea01/app/src/main/java/com/example/tarea01/MainActivity.kt
@@ -15,32 +15,8 @@ import com.example.tarea01.ui.theme.Tarea01Theme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            Tarea01Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Charlie")
-                }
-            }
-        }
+        setContentView(R.layout.layout)
+
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Tarea01Theme {
-        Greeting("Android")
-    }
-}
