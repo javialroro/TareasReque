fcaa7305219afa195e36f8c790cd3c3659e9bb00
ScarloVM <cvenegasm1043@gmail.com>
2024-02-21 13:31:22 -0600

    Clase Producto
    Se agrego la clase producto

---
+++ Tarea01/app/src/main/java/com/example/tarea01/Producto.kt
@@ -0,0 +1,10 @@
+package com.example.tarea01
+
+class Producto {
+    var id: String = ""
+    var Nombre: String = ""
+    var cantVendidos: Int = 0
+    var precioCosto: Int = 0
+    var precioVenta: Int = 0
+    var cantidad: Int = 0
+}
