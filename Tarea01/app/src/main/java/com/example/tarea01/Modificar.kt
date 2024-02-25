e65bdb25d8b6549fb2b7fb61c859374570dfe8f1
ScarloVM <cvenegasm1043@gmail.com>
2024-02-24 19:27:13 -0600

    Modificar
    Se ha realizado el funcionamiento de modificar los productos

--- Tarea01/app/src/main/java/com/example/tarea01/Modificar.kt
+++ Tarea01/app/src/main/java/com/example/tarea01/Modificar.kt
@@ -2,10 +2,47 @@ package com.example.tarea01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import com.google.android.material.textfield.TextInputEditText
 
 class Modificar : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_modificar)
+    }
+
+    fun modificarProducto(view: View) {
+
+        // Obtener el ID del producto a modificar
+        val id = findViewById<TextInputEditText>(R.id.idProducto).text.toString()
+        val nuevoPrecioCosto = findViewById<TextInputEditText>(R.id.precioCosto).text.toString().toIntOrNull()
+        val nuevoPrecioVenta = findViewById<TextInputEditText>(R.id.precioVenta).text.toString().toIntOrNull()
+        val nuevaCantidad = findViewById<TextInputEditText>(R.id.cantidad).text.toString().toIntOrNull()
+
+        // Buscar el producto en la base de datos
+        val producto = ProductosSingleton.productos.find { it.id == id }
+
+        // Si el producto existe, eliminarlo
+        if (producto != null) {
+            if (nuevoPrecioCosto != null)
+                producto.precioCosto = nuevoPrecioCosto
+            if (nuevoPrecioVenta != null)
+                producto.precioVenta = nuevoPrecioVenta
+            if (nuevaCantidad != null)
+                producto.cantidad = nuevaCantidad
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Success")
+            builder.setMessage("Producto modificado correctamente")
+            builder.setPositiveButton("OK", null)
+            builder.show()
+        } else {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Error")
+            builder.setMessage("No se encontró un producto con el ID especificado")
+            builder.setPositiveButton("OK", null)
+            builder.show()
+        }
     }
 }
--- Tarea01/app/src/main/java/com/example/tarea01/Producto.kt
+++ Tarea01/app/src/main/java/com/example/tarea01/Producto.kt
@@ -11,6 +11,12 @@ class Producto(
     fun imprimirDetalles(): String {
         return "Nombre: $Nombre\nID: $id\nCantidad vendidos: $cantVendidos\nPrecio costo: $precioCosto\nPrecio venta: $precioVenta\nCantidad: $cantidad"
     }
+
+    fun modificarDatos(nuevoPrecioCosto: Int, nuevoPrecioVenta: Int, nuevaCantidad: Int) {
+        this.precioCosto = nuevoPrecioCosto
+        this.precioVenta = nuevoPrecioVenta
+        this.cantidad = nuevaCantidad
+    }
     // Este es el constructor primario de la clase Producto
     // Los parámetros se declaran directamente en la declaración de la clase
     // No necesitas definir un constructor explícito
