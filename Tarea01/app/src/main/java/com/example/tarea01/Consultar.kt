09e88bcce76dda95a42cc0e1a595ad15dfdcae83
ScarloVM <cvenegasm1043@gmail.com>
2024-02-24 20:59:01 -0600

    Actualizacion de Consultar
    Se actualizo Consultar para que mostrara todos los productos o los filtrara por precio de venta, cantidad o cantidad de vendidos

--- Tarea01/app/src/main/java/com/example/tarea01/Consultar.kt
+++ Tarea01/app/src/main/java/com/example/tarea01/Consultar.kt
@@ -12,18 +12,22 @@ class Consultar : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_consultar)
     }
-    fun consultarProducto(view: View) {
+    fun consultarProductos(view: View) {
         // Obtener el ID del producto a eliminar
 
         val id = findViewById<TextInputEditText>(R.id.id).text.toString()
 
         // Buscar el producto en la base de datos
-        val producto = ProductosSingleton.productos.find { it.id == id }
+        val productos = ProductosSingleton.productos
 
         // Si el producto existe, eliminarlo
-        if (producto != null) {
+        if (productos.isNotEmpty()) {
 
-            findViewById<TextView>(R.id.eliminadoText).text = "El producto consultado fue: \n"+producto.imprimirDetalles()
+            var text: String = ""
+            for (producto in productos) {
+                text += producto.imprimirDetalles() + "\n\n"
+            }
+            findViewById<TextView>(R.id.eliminadoText).text = text
             val builder = AlertDialog.Builder(this)
             builder.setTitle("Success")
             builder.setMessage("Producto consultado correctamente")
@@ -33,7 +37,118 @@ class Consultar : AppCompatActivity() {
         } else {
             val builder = AlertDialog.Builder(this)
             builder.setTitle("Error")
-            builder.setMessage("No se encontró un producto con el ID especificado")
+            builder.setMessage("No existen productos")
+            builder.setPositiveButton("OK", null)
+            builder.show()
+        }
+    }
+
+    fun consultarProductoPorPrecio(view: View) {
+        // Obtener el ID del producto a eliminar
+
+        val precioVenta = findViewById<TextInputEditText>(R.id.id).text.toString().toIntOrNull()
+
+        if (precioVenta != null) {
+            // Buscar los productos en la base de datos que coincidan con el precio de venta
+            val productos = ProductosSingleton.productos.filter { it.precioVenta == precioVenta }
+
+            // Si el producto existe, eliminarlo
+            if (productos.isNotEmpty()) {
+                var text: String = ""
+                for (producto in productos) {
+                    text += producto.imprimirDetalles() + "\n\n"
+                }
+                findViewById<TextView>(R.id.eliminadoText).text = text
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle("Success")
+                builder.setMessage("Productos consultados correctamente")
+                builder.setPositiveButton("OK", null)
+                builder.show()
+            } else {
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle("Error")
+                builder.setMessage("No existen productos con el precio indicado")
+                builder.setPositiveButton("OK", null)
+                builder.show()
+            }
+        } else {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Error")
+            builder.setMessage("Indique el precio que desea consultar")
+            builder.setPositiveButton("OK", null)
+            builder.show()
+        }
+    }
+
+    fun consultarProductoPorCantidad(view: View) {
+        // Obtener el ID del producto a eliminar
+
+        val Cantidad = findViewById<TextInputEditText>(R.id.id).text.toString().toIntOrNull()
+
+        if (Cantidad != null) {
+            // Buscar los productos en la base de datos que coincidan con el precio de venta
+            val productos = ProductosSingleton.productos.filter { it.cantidad == Cantidad }
+
+            // Si el producto existe, eliminarlo
+            if (productos.isNotEmpty()) {
+                var text: String = ""
+                for (producto in productos) {
+                    text += producto.imprimirDetalles() + "\n\n"
+                }
+                findViewById<TextView>(R.id.eliminadoText).text = text
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle("Success")
+                builder.setMessage("Productos consultados correctamente")
+                builder.setPositiveButton("OK", null)
+                builder.show()
+            } else {
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle("Error")
+                builder.setMessage("No existen productos con la cantidad indicada")
+                builder.setPositiveButton("OK", null)
+                builder.show()
+            }
+        } else {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Error")
+            builder.setMessage("Indique la cantidad que desea consultar")
+            builder.setPositiveButton("OK", null)
+            builder.show()
+        }
+    }
+
+    fun consultarProductoPorCantidadVendidos(view: View) {
+        // Obtener el ID del producto a eliminar
+
+        val CantVendidos = findViewById<TextInputEditText>(R.id.id).text.toString().toIntOrNull()
+
+        if (CantVendidos != null) {
+            // Buscar los productos en la base de datos que coincidan con el precio de venta
+            val productos = ProductosSingleton.productos.filter { it.cantVendidos == CantVendidos }
+
+            // Si el producto existe, eliminarlo
+            if (productos.isNotEmpty()) {
+                var text: String = ""
+                for (producto in productos) {
+                    text += producto.imprimirDetalles() + "\n\n"
+                }
+                findViewById<TextView>(R.id.eliminadoText).text = text
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle("Success")
+                builder.setMessage("Productos consultados correctamente")
+                builder.setPositiveButton("OK", null)
+                builder.show()
+            } else {
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle("Error")
+                builder.setMessage("No existen productos con la cantidad indicada")
+                builder.setPositiveButton("OK", null)
+                builder.show()
+            }
+        } else {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Error")
+            builder.setMessage("Indique la cantidad que desea consultar")
             builder.setPositiveButton("OK", null)
             builder.show()
         }
