8594a33359456f31bd5cf0e23887c4d4226fa4c3
ScarloVM <cvenegasm1043@gmail.com>
2024-03-16 15:22:00 -0600

    Menu Hamburguesa
    Se implemento la funcion de un menu hamburguesa

--- Tarea01/app/src/main/java/com/example/tarea01/MainActivity.kt
+++ Tarea01/app/src/main/java/com/example/tarea01/MainActivity.kt
@@ -3,20 +3,74 @@ package com.example.tarea01
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 import java.io.Serializable
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navView = findViewById(R.id.nav_view)
+
+        // Configurar el icono de hamburguesa
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_menu)
+
+        drawerLayout.addDrawerListener(object : DrawerLayout.DrawerListener {
+            override fun onDrawerSlide(drawerView: View, slideOffset: Float) {}
+            override fun onDrawerOpened(drawerView: View) {}
+            override fun onDrawerClosed(drawerView: View) {}
+            override fun onDrawerStateChanged(newState: Int) {}
+        })
+
+        // Manejar la apertura y cierre del menú hamburguesa
+        navView.setNavigationItemSelectedListener { menuItem ->
+            // Acciones al hacer clic en un elemento del menú
+            when (menuItem.itemId) {
+                R.id.nav_home -> {
+                    // Acción para el elemento "Home"
+                    Toast.makeText(this, "Home seleccionado", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.nav_gallery -> {
+                    // Acción para el elemento "Gallery"
+                    Toast.makeText(this, "Gallery seleccionado", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.nav_slideshow -> {
+                    // Acción para el elemento "Slideshow"
+                    Toast.makeText(this, "Slideshow seleccionado", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
+
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                // Abrir el DrawerLayout al hacer clic en el icono de hamburguesa o en el botón de retroceso
+                drawerLayout.openDrawer(GravityCompat.START)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     fun abrirAgregar(view: View){
